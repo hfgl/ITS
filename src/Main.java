151dd9d1ffafe1687be1a7710073da827292b6f2
Till Pohland <till.pohland@gmx.de>
2017-06-08 12:46:30 +0200

    Forgot more

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 
 import Aufgabe1.LCG;
 import Aufgabe2.HC1;
-import Aufgabe3.TripleDES;
-import Aufgabe3.TripleDES_CFB_Copy;
+import Aufgabe3.TripleDES_CFB;
 
 public class Main 
 {
@@ -22,12 +21,22 @@ public class Main
 			HC1.CipherLCG("src/testLCG.txt", 65489432);
 			HC1.CipherSecureRandom("src/testRandom.txt", 65489432);
 
+
+
 			//Aufgabe3
-			TripleDES_CFB_Copy tripleDES_cfb = new TripleDES_CFB_Copy();
+			TripleDES_CFB tripleDES_cfb = new TripleDES_CFB();
+			tripleDES_cfb.Cipher(
+					System.getProperty("user.dir") + "\\src\\Aufgabe3\\ITSAufgabe3.pdf",
+					System.getProperty("user.dir") + "\\src\\Aufgabe3\\3DESTest.key",
+					System.getProperty("user.dir") + "\\src\\Aufgabe3\\3DESTest.enc",
+					"encrypt"
+			);
+
+			tripleDES_cfb = new TripleDES_CFB();
 			tripleDES_cfb.Cipher(
 				System.getProperty("user.dir") + "\\src\\Aufgabe3\\3DESTest.enc",
 				System.getProperty("user.dir") + "\\src\\Aufgabe3\\3DESTest.key",
-				System.getProperty("user.dir") + "\\src\\Aufgabe3\\3DESTest.out",
+				System.getProperty("user.dir") + "\\src\\Aufgabe3\\3DESTest.pdf",
 				"decrypt"
 			);
 
